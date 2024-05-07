35e58d797837f114af926d6d24d715530b99aa96
braian <brasanluc123@gmail.com>
2024-05-06 21:42:17 -0300

    clase 179; creando el menu principal y dandole estilos

--- android/app/src/main/java/com/componentapp/MainApplication.kt
+++ android/app/src/main/java/com/componentapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.componentsapps
+package com.componentapp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {View, Text} from 'react-native';
 
 export const ComponentsApp = () => {
   return (
-    <View>
-      <Text>ComponentsApp</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>ComponentsApp</Text>
+      </View>
+    </NavigationContainer>
   );
 };
