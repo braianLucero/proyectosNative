163f3c4b19fb68fbafa9e187577f78e5d69d09f2
braian <brasanluc123@gmail.com>
2024-05-06 22:48:26 -0300

    class 185. creating custom Components

---
+++ src/presentation/screen/switches/SwitchScreen.tsx
@@ -0,0 +1,13 @@
+import {CustomView} from '../../components/ui/CustomView';
+import {Card} from '../../components/ui/Card';
+import {Button} from 'react-native-paper';
+
+export const SwitchScreen = () => {
+  return (
+    <CustomView style={{marginTop: 100, paddingHorizontal: 10}}>
+      <Card>
+        <Button text="Click Men" onPress={() => {}} />
+      </Card>
+    </CustomView>
+  );
+};
