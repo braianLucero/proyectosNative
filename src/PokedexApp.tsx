6cfaddaa35919487dc10c6eb079e91b6b81904e2
braian <brasanluc123@gmail.com>
2024-05-09 21:04:14 -0300

    225. TanStack Query - Peticiones y caché

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,12 +1,17 @@
 import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import React, {PropsWithChildren} from 'react';
-import {PaperProvider} from 'react-native-paper';
 
-export const PokedexApp = ({children}: PropsWithChildren) => {
+import {StackNavigator} from './presentation/navigator/StackNavigator';
+import {ThemeContextProvider} from './presentation/context/ThemeContext';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+export const PokedexApp = () => {
   return (
-    <PaperProvider>
-      <NavigationContainer>{children}</NavigationContainer>;
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContextProvider>
+        <StackNavigator />
+      </ThemeContextProvider>
+    </QueryClientProvider>
   );
 };
