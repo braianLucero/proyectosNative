f9715f103db99ec0da58705fc8ef723bed6b3d3f
braian <brasanluc123@gmail.com>
2024-05-14 23:28:16 -0300

    class 261. ; Loading Screen - Waiting screen

--- src/presentation/screens/permissions/PermissionsScreen.tsx
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,26 +1,21 @@
-import { Pressable, Text, View } from 'react-native';
-import { globalStyles } from '../../../config/theme/styles';
-import { usePermissionStore } from '../../store/permissions/usePermissionStore';
+import {Pressable, Text, View} from 'react-native';
+import {globalStyles} from '../../../config/theme/styles';
+import {usePermissionStore} from '../../store/permissions/usePermissionStore';
 
 export const PermissionsScreen = () => {
-
-  const { locationStatus, requestLocationPermission } = usePermissionStore();
-
+  const {locationStatus, requestLocationPermission} = usePermissionStore();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Habilitar ubicación</Text>
 
       <Pressable
-        style={ globalStyles.btnPrimary }
-        onPress={ requestLocationPermission }
-      >
-        <Text style={{ color: 'white' }}>Habilitar Localización</Text>
+        style={globalStyles.btnPrimary}
+        onPress={requestLocationPermission}>
+        <Text style={{color: 'white'}}>Habilitar Localización</Text>
       </Pressable>
 
-
-      <Text>Estado actual: { locationStatus }</Text>
-      
+      <Text>Estado actual: {locationStatus}</Text>
     </View>
-  )
-}
+  );
+};
