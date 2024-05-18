c0ec3aefc734675f54356111401f3a35d8872457
braian <brasanluc123@gmail.com>
2024-05-17 23:18:06 -0300

    class 302. ; AuthProvider

--- src/presentation/screen/loading/LoadingScreen.tsx
+++ src/presentation/screen/loading/LoadingScreen.tsx
@@ -1,9 +1,10 @@
-import {ActivityIndicator, Text, View} from 'react-native';
+import {Layout, Spinner} from '@ui-kitten/components';
+import {Text, View} from 'react-native';
 
 export const LoadingScreen = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <ActivityIndicator size={30} color="black" />
-    </View>
+    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Spinner status="primary" size="large" />
+    </Layout>
   );
 };
